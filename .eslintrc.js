86828165bec3df805071a8b46ae25d7a07391baa
OYsun <web-oysun@qq.com>
2017-02-01 19:26:15 +0800

    V0.0.4
    - 重写vwebpack配置，增加eslint校验代码风格
    - 修复移动端点击有边框阴影

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 6, 
+    sourceType: 'module'
+  },
+  extends: 'standard', 
+  plugins: [
+    'html' 
+  ],
+  'rules': {
+    
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,53 @@
-var options = require('./webpack.base.js');
-    options.entry = './src';
-    options.output = {
-      library: 'VueStar',
-      libraryTarget: 'umd',
-      filename: 'VueStar.js',
-      path: './dist'
-    };
-    options.externals = {
-      vue: 'Vue'
-    };
-module.exports = options;
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    library: 'VueStar',
+    libraryTarget: 'umd',
+    path: './dist', 
+    filename: 'VueStar.js' 
+  },
+  externals: {
+    vue: 'Vue'
+  },
+  eslint: {
+    formatter: require('eslint-friendly-formatter')
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.vue$/,
+        loader: 'eslint',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.js$/,
+        loader: 'eslint',
+        exclude: /node_modules/
+      }
+    ],
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      }, 
+      {
+        test: /\.css$/,
+        loader: 'style!css!autoprefixer'
+      }, 
+      {
+        test: /\.less$/,
+        loader: 'style!css!less!autoprefixer'
+      }
+    ]
+  },
+  vue: {
+    postcss: [
+      require('autoprefixer')()
+    ],
+  }
+}
